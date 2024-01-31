6ff96a5369344e02eebeb677fc8ffb92beb9412e
ricardoalexand <126792060+ricardoalexand@users.noreply.github.com>
2024-01-31 22:36:23 +0000

    new changes

--- arrays.js
+++ arrays.js
@@ -6,43 +6,43 @@ let age = document.querySelector(".age");
 
 // CODE ALONG
 // 1. Update the variable fruitList so that it is an array with at least 4 fruits.
-let fruitList = [];
+let fruitList = ["berries", "strawberries", "watermelons", "oranges"];
 console.log();
 
 // 2. Update the variable flowerList so that it is an array with at least 4 flowers.
-let flowerList = [];
+let flowerList = ["sunflowers", "orchids", "tulips", "larkspurs"];
 console.log();
 
 // 3. Update the variable colorList so that it is an array with at least 4 different colors.
-let colorList = [];
+let colorList = ["blue", "green", "gold", "grey"];
 console.log();
 
 // 4. Update the variable ageList so that it is an array with a list of ages (numbers) of classmates and volunteers.
-let ageList = [];
+let ageList = [15, 16, 17, 18];
 console.log();
 
 
 // CODE SOLO
 // 5. Update the variable favFruit to retrieve the array element of your favorite fruit in the fruitList array. 
 // 6. Update the innerHTML to the favFruit variable.
-let favFruit = '';
-fruit.innerHTML = '';
+let favFruit = fruitList[1];
+fruit.innerHTML = favFruit;
 
 
 // 7. Update the variable favFlower to retrieve the array element of your favorite flower in the flowerList array. 
 // 8. Update the innerHTML to the favFlower variable.
-let favFlower = '';
-flower.innerHTML = '';
+let favFlower = flowerList[2];
+flower.innerHTML = favFlower;
 
 // 9. Update the variable favColor to retrieve your favorite color from the colorList array. 
 // 10.Update the innerHTML to the favColor variable.
-let favColor = '';
-color.innerHTML = '';
+let favColor = colorList[0];
+color.innerHTML = favColor;
 
 
 // 11. Update the variable oldestAge to retrieve the array element of the oldest person in the ageList array. 
 // 12. Update the innerHTML to the oldestAge variable.
-let oldestAge = '';
-age.innerHTML = '';
+let oldestAge = ageList[3];
+age.innerHTML = oldestAge;
 
 
